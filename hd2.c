f62e5a49990da3a4dfdf4da85149e1d130515fa7
Kamil Braun <kb346840@students.mimuw.edu.pl>
2019-05-10 19:11:42 +0000

    character device

--- hd2.c
+++ hd2.c
@@ -2,31 +2,68 @@
 #include <linux/kernel.h>
 #include "harddoom2.h"
 
+MODULE_LICENSE("GPL");
+
 #define DEBUG(fmt, ...) printk(KERN_INFO "hd2: " fmt "\n", ##__VA_ARGS__)
 #define ERROR(fmt, ...) printk(KERN_ERR "hd2: " fmt "\n", ##__VA_ARGS__)
 #define DRV_NANE "harddoom2_driver"
+#define CHRDEV_NAME "doom"
+#define DEVICES_LIMIT 256
 
 static const struct pci_device_id pci_ids[] = {
     { PCI_DEVICE(HARDDOOM2_VENDOR_ID, HARDDOOM2_DEVICE_ID), },
     { /* end: all zeroes */ },
 };
 
-struct drvdata {
-    void __iomem* bar;
+static struct class doom_class = {
+    .name = CHRDEV_NAME,
+    .owner = THIS_MODULE,
 };
 
-int drv_probe(struct pci_dev* dev, const struct pci_device_id* id) {
-    int err = 0;
-    struct drvdata* data = NULL;
+struct device_data {
+    int number;
+    void __iomem* bar;
+    struct cdev cdev;
+};
 
-    DEBUG("probe called: %#010x, %#0x10x\n", id->vendor, id->device);
+static struct device_data devices[DEVICES_LIMIT];
+
+static struct file_operations doom_ops = {
+    .owner = THIS_MODULE,
+    .open = doom_open,
+    .release = doom_release,
+    .unlocked_ioctl = doom_ioctl,
+    .compat_ioctl = doom_ioctl,
+    .write = doom_write
+};
+
+static int dev_counter = 0;
+static DEFINE_MUTEX(dev_counter_mut);
+
+int alloc_dev_number() {
+    int ret = -ENOSPC;
+
+    /* TODO: interruptible lock? */
+    mutex_lock(&dev_counter_mut);
+    if (dev_counter < DEVICES_LIMIT) {
+        ret = dev_counter++;
+    }
+    mutex_unlock(&dev_counter_mut);
+    return ret;
+}
+
+static int pci_probe(struct pci_dev* dev, const struct pci_device_id* id) {
+    int err = 0;
+    void __iomem bar* = NULL;
+    struct device* device = NULL;
+    struct device_data* data = NULL;
+
+    DEBUG("probe called: %#010x, %#0x10x", id->vendor, id->device);
 
     if (id->vendor != HARDDOOM2_VENDOR_ID || id->device != HARDDOOM2_DEVICE_ID) {
-        DEBUG("Wrong device_id!\n");
+        DEBUG("Wrong device_id!");
         return -EINVAL;
     }
-
-    /* TODO: init, multiple devices? */
 
     if ((err = pci_enable_device(dev))) {
         DEBUG("failed to enable device");
@@ -38,17 +75,8 @@ int drv_probe(struct pci_dev* dev, const struct pci_device_id* id) {
         goto out_regions;
     }
 
-    data = kmalloc(sizeof(drvdata), GFP_KERNEL);
-    if (!data) {
-        DEBUG("failed to kmalloc drvdata");
-        err = -ENOMEM;
-        goto out_drvdata;
-    }
-
-    pci_set_drvdata(dev, data);
-
-    data->bar = iomap(dev, 0, 0);
-    if (!data->bar) {
+    bar = iomap(dev, 0, 0);
+    if (!bar) {
         DEBUG("can't map register space");
         err = -ENOMEM;
         goto out_iomap;
@@ -67,17 +95,51 @@ int drv_probe(struct pci_dev* dev, const struct pci_device_id* id) {
         goto out_dma;
     }
 
+    /* TODO: free dev numbers, use list? */
+    dev_number = alloc_dev_number();
+    if (dev_number < 0) {
+        DEBUG("failed to allocate device number");
+        err = dev_number;
+        goto out_dma;
+    }
+    if (dev_number >= DEVICE_LIMIT) { ERROR("OOPS"); err = -ENOSPC; goto out_dma; }
+
+    data = &devices[dev_number];
+    pci_set_drvdata(dev, data);
+
+    data->number = dev_number;
+    data->bar = bar;
+    cdev_init(&data->cdev, &doom_ops);
+    data->cdev.owner = THIS_MODULE;
+
+    /* TODO: setup device before cdev_add */
+
+    if ((err = cdev_add(&data->cdev, doom_major + dev_number, 1))) {
+        DEBUG("can't register cdev");
+        goto out_cdev_add;
+    }
+
+    /* TODO: store device? */
+    device = device_create(&doom_class, dev->dev, doom_major + dev_number, 0, CHRDEV_NAME "%d", dev_number);
+    if (IS_ERR(device)) {
+        DEBUG("can't create device");
+        err = PTR_ERR(device);
+        goto err_device;
+    }
+
     /* TODO interrupt handling */
 
     return 0;
 
+err_device:
+    cdev_del(&data->cdev);
+out_cdev_add:
+    pci_set_drvdata(dev, NULL);
+    /* TODO */
 out_dma:
     pci_clear_master(dev);
     pci_iounmap(dev, data->bar);
 out_iomap:
-    pci_set_drvdata(dev, NULL);
-    kfree(data);
-out_drvdata:
     pci_release_regions(dev);
 out_regions:
     pci_disable_device(dev);
@@ -85,69 +147,109 @@ out_enable:
     return err;
 }
 
-void drv_remove(struct pci_dev* dev) {
-    void __iomem* data;
+static void pci_remove(struct pci_dev* dev) {
+    struct device_data* data = pci_get_drvdata(dev);
+    void __iomem* bar = NULL;
 
-    /* TODO: cleanup */
-    pci_clear_master(dev);
-    data = pci_get_drvdata(dev);
     if (!data) {
-        ERROR("drv_remove: no drvdata!");
+        ERROR("pci_remove: no pcidata!");
+    } else {
+        device_destroy(&doom_class, doom_major + data->number);
+        cdev_del(&data->cdev);
+        bar = data->bar;
+        /* TODO: free dev number */
     }
-    if (!data->bar) {
-        ERROR("drv_remove: no bar!");
-    }
-    pci_iounmap(dev, data->bar);
+
     pci_set_drvdata(dev, NULL);
-    kfree(data);
+    pci_clear_master(dev);
+
+    if (!bar) {
+        ERROR("pci_remove: no bar!");
+    } else {
+        pci_iounmap(dev, data->bar);
+    }
+
     pci_release_regions(dev);
     pci_disable_device(dev);
 }
 
-int suspend(struct pci_dev* dev, pm_message_t state) {
+static int pci_suspend(struct pci_dev* dev, pm_message_t state) {
     /* TODO cleanup */
     return 0;
 }
 
-int drv_resume(struct pci_dev* dev) {
+static int pci_resume(struct pci_dev* dev) {
     /* TODO: resume */
 }
 
-void drv_shutdown(struct pci_dev* dev) {
+static void pci_shutdown(struct pci_dev* dev) {
     /* TODO cleanup */
 }
 
-static const struct pci_driver hd2_drv = {
+static const struct pci_driver pci_drv = {
     .name = DRV_NAME,
     .id_table = pci_ids,
-    .probe = drv_probe,
-    .remove = drv_remove,
-    .suspend = drv_suspend,
-    .resume = drv_resume,
-    .shutdown = drv_shutdown
+    .probe = pci_probe,
+    .remove = pci_remove,
+    .suspend = pci_suspend,
+    .resume = pci_resume,
+    .shutdown = pci_shutdown
 };
 
-MODULE_LICENSE("GPL");
+static dev_t doom_major;
 
-int hd2_init(void)
+static int doom_open(struct inode* inode, struct file* file) {
+    /* TODO */
+}
+
+static int doom_release(struct inode* inode, struct file* file) {
+    /* TODO */
+}
+
+static int doom_ioctl(struct file* file, unsigned cmd, unsigned long arg) {
+    /* TODO */
+}
+
+static ssize_t doom_write(struct file* file, const char __user *buf, size_t count, loff_t* off) {
+    /* TODO */
+}
+
+static int hd2_init(void)
 {
-    int err;
-	DEBUG("Init\n");
-    if ((err = pci_register_driver(&hd2_drv))) {
-        DEBUG("Failed to register driver\n");
+    int err = 0;
+
+	DEBUG("Init");
+    if ((err = alloc_chrdev_region(&doom_major, DEVICES_LIMIT, CHRDEV_NAME))) {
+        DEBUG("failed to alloc chrdev region");
+        goto out_alloc_reg;
+    }
+
+    if ((err = class_register(&doom_class))) {
+        DEBUG("failed to register class");
+        goto out_class;
+    }
+
+    if ((err = pci_register_driver(&pci_drv))) {
+        DEBUG("Failed to register pci driver");
         goto out_reg_drv;
     }
 
 	return 0;
 
 out_reg_drv:
+    class_unregister(&doom_class);
+out_class:
+    unregister_chrdev_region(&doom_major, DEVICES_LIMIT);
+out_alloc_reg:
     return err;
 }
 
-void hd2_cleanup(void)
+static void hd2_cleanup(void)
 {
-	DEBUG("Cleanup\n");
-    pci_unregister_driver(&hd2_drv);
+	DEBUG("Cleanup");
+    pci_unregister_driver(&pci_drv);
+    class_unregister(&doom_class);
+    unregister_chrdev_region(doom_major, DEVICES_LIMIT);
 }
 
 module_init(hd2_init);
