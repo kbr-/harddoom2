62847a4ff82cb115248d181599c6ba4a2477119d
Kamil Braun <kb346840@students.mimuw.edu.pl>
2019-05-22 21:52:40 +0000

    disable debug

--- common.h
+++ common.h
@@ -1,6 +1,8 @@
 #include "harddoom2.h"
 
-#define DEBUG(fmt, ...) printk(KERN_INFO "hd2: " fmt "\n", ##__VA_ARGS__)
+// #define DEBUG(fmt, ...) printk(KERN_INFO "hd2: " fmt "\n", ##__VA_ARGS__)
+#define DEBUG(fmt, ...)
+
 #define ERROR(fmt, ...) printk(KERN_ERR "hd2: " fmt "\n", ##__VA_ARGS__)
 
 #define DRV_NAME "HardDoom_][™"
